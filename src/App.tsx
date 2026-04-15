b23becc80feab729e9e042ca324d19538f083230
mdhanush66 <m.dhanush666dhanu@gmail.com>
2026-04-16 00:15:51 +0530

    chore : base url update

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/bawanthika-birthday">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="*" element={<NotFound />} />
